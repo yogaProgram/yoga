84ae9b2fe41f2bd081e01d95e536250e284c257c
kun Yuan <kun Yuan>
2016-07-18 14:34:48 +0800

    add yogaProgram

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+  // $(".course-type").mouseover(function(){
+  //   $(".description").animate({
+  //     height:'100%',
+  //     backgroundColor:'red'
+  //   });
+  // });
+$("#activi-ing").click(function(){
+  $(".filter-time-child").fadeToggle();
+});
